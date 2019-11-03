0ec44c1f8d0bdeda158cc0b0dddad155725455e7
Sujay Shah <svshah047@gmail.com>
2019-11-03 16:18:02 -0600

    Updated packages

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  
+
+  constructor() {
+
+  }
+
+  onSubmit(form) {
+    console.log(form);
+  }
 }
