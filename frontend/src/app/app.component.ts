81e48c35b9da2a2f80adaa2f5c4b8ca514af19c5
Sujay Shah <svshah047@gmail.com>
2019-11-04 18:36:59 -0600

    Frontend changes

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+export interface Tile {
+  color: string;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,10 +15,25 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  tiles: Tile[] = [
+    {text: '1', color: 'lightblue'},
+    {text: '2', color: 'lightgreen'},
+    {text: '3', color: 'lightpink'},
+    {text: '4', color: '#DDBDF1'},
+  ];
+  defaultTiles = Object.assign([], this.tiles);
 
   constructor() {
 
+  }
+  
+  addColor(color: string) {
+    let newTile : Tile = {color: color, text: (this.tiles.length + 1).toString()};
+    this.tiles.push(newTile);
+  }
+
+  resetDefault() {
+    this.tiles = Object.assign([], this.defaultTiles);;
   }
 
   onSubmit(form) {
